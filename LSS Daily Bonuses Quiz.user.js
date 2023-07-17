c227c987a02fd8521ae4d5379e849aaeb3674bcf
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-07-17 17:53:01 +0200

    Update LSS Daily Bonuses Quiz.user.js
    +fixed Question not showing on darkmode

--- LSS Daily Bonuses Quiz.user.js
+++ LSS Daily Bonuses Quiz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS Daily Bonuses Quiz
 // @namespace    https://www.leitstellenspiel.de
-// @version      1.7
+// @version      1.8
 // @description  Popup quiz for the daily bonuses
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/daily_bonuses
@@ -844,6 +844,7 @@
     var frageElement = document.createElement("p");
     frageElement.style.fontSize = "24px";
     frageElement.style.textAlign = "center";
+    frageElement.style.color = "black";
     popupContent.appendChild(frageElement);
 
     // Antwortbuttons Container erstellen
